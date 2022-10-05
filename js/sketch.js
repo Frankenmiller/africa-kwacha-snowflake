682bd170d8e061dc4257b8a8e06f1fb56e35e9aa
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-10-05 11:22:21 +0200

    repository initialized

---
+++ js/sketch.js
@@ -0,0 +1,42 @@
+let snow = [];
+let gravity;
+let spritesheet;
+let textures = [];
+
+function preload() {
+    spritesheet = loadImage('images/kwacha_spritesheet.png');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    gravity = createVector(0, 0.01);
+    
+    for (let x=0; x<408; x+=102) {
+        for (let y=0; y<147; y+=49) {
+            let img = spritesheet.get(x, y, 102, 49);
+            image(img, x, y);
+            textures.push(img);
+        }
+    }
+
+    for (let i=0; i<180; i++) {
+        let x = random(width);
+        let y = random(height);
+        let design = random(textures);
+        snow.push(new Snowflake(x, y, design));
+    }    
+}
+
+function draw() {
+    background(0);
+
+    for (flake of snow) {
+        flake.applyForce(gravity);
+        flake.update();
+        flake.render();
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
---
+++ js/snowflake.js
@@ -0,0 +1,68 @@
+function getRandomSize() {
+
+    let r = pow(random(0.5, 1), 5);
+    return constrain(r * 36, 2, 36);
+
+}
+
+class Snowflake {
+    constructor(sx, sy, img) {
+        let x = sx || random(width);
+        let y = sy || random(-100, -10);
+        this.img = img;
+        this.pos = createVector(x, y);
+        this.vel = createVector(0, 0.0);
+        this.acc = createVector();
+        this.angle = random(TWO_PI);
+        this.dir = (random(1) > 0.5) ? 1 : -1;
+        this.r = getRandomSize();
+        this.xOff = 0;
+    }
+    
+    applyForce(force) {
+        let f = force.copy();
+        f.mult(this.r);
+        
+        // let f = force.copy();
+        // f.div(this.mass);
+        this.acc.add(f);
+    }
+    
+    randomize() {        
+        let x = random(width);
+        let y = random(-100, -50);
+        this.pos = createVector(x, y);
+        this.vel = createVector(0, 0.0);
+        this.acc = createVector();
+        this.r = getRandomSize();
+    }
+
+    update() {
+        this.xOff = sin(this.angle) * this.r;
+        this.vel.add(this.acc);
+        this.vel.limit(this.r * 0.2);
+        if (this.vel.mag() < 1) {
+            this.vel.normalize();
+        }
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+        
+        if (this.pos.y > height + 150) {
+            this.randomize();
+        }
+
+        this.angle += this.dir * this.vel.mag() / 100;
+
+    }
+
+    render() {
+        push();
+        translate(this.pos.x + this.xOff, this.pos.y);
+        rotate(this.angle);
+        imageMode(CENTER);
+        image(this.img, 0, 0, 102, 49);
+        pop();
+        
+    }
+
+}
