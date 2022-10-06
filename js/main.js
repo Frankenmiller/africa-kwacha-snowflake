83fecb98b1672833287882436d55680417d65069
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-10-06 15:25:29 +0200

    Burger button triggering scroll, scroll menu buttons displaying properly with hover effects

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 window.onload = function () {
-    const menu_btn = document.querySelector('.hamburger');
+    const menu_button = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
 
-    menu_btn.addEventListener('click', function () {
-        menu_btn.classList.toggle('is-active');
+    menu_button.addEventListener('click', function () {
+        menu_button.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
     });
 }
